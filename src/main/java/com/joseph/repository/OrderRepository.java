d8e83433d155331dc09cd427dd193fea5dcb5129
yassine jouaoudi <yass.jouaoudi@gmail.com>
2024-01-04 10:45:41 +0100

    add tests orders functions

--- src/main/java/com/joseph/repository/OrderRepository.java
+++ src/main/java/com/joseph/repository/OrderRepository.java
@@ -1,24 +1,20 @@
 package com.joseph.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.joseph.Model.rapportData;
 import com.joseph.entity.Order;
 
-import java.time.LocalDate;
-import java.util.List;
-
 @Repository("orderRepository")
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    @Query("SELECT NEW com.joseph.Model.rapportData(o.dateorder, SUM(o.totalorder)) " +
-            "FROM Order o " +
-            "WHERE o.dateorder BETWEEN :startDate AND :endDate " +
-            "GROUP BY o.dateorder " +
-            "ORDER BY o.dateorder")
-    List<rapportData> getTotalOrdersBetweenDates(@Param("startDate") LocalDate startDate,
-            @Param("endDate") LocalDate endDate);
+	@Query("SELECT NEW com.joseph.Model.rapportData(o.dateorder, SUM(o.totalorder)) " + "FROM Order o "
+			+ "WHERE o.dateorder BETWEEN :startDate AND :endDate " + "GROUP BY o.dateorder " + "ORDER BY o.dateorder")
+	List<rapportData> getTotalOrdersBetweenDates(@Param("startDate") LocalDate startDate,
+			@Param("endDate") LocalDate endDate);
 }
--- src/test/java/com/joseph/service/Impl/OrderServiceImplTest.java
+++ src/test/java/com/joseph/service/Impl/OrderServiceImplTest.java
@@ -1,23 +1,27 @@
 package com.joseph.service.Impl;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import java.time.LocalDate;
 import java.util.List;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.joseph.Model.rapportData;
 import com.joseph.config.PersistenceJPAConfig;
 import com.joseph.entity.Client;
 import com.joseph.entity.Order;
 import com.joseph.entity.Status;
 import com.joseph.repository.OrderRepository;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 
@@ -30,8 +34,54 @@ class OrderServiceImplTest {
 	private OrderServiceImpl orderServiceImpl;
 
 	@BeforeEach
-	void setuptest() {
+	@DisplayName("Setup Tests for Orders")
+	void setupTest() {
+		Client c = new Client();
+		c.setIdclient(1);
+		Order o1 = new Order();
+		o1.setDateorder(LocalDate.now());
+		o1.setStatusorder(Status.INPROGRESS);
+		o1.setClient(c);
+		o1.setTotalorder(300.0);
+		o1 = orderRepository.save(o1);
+		Order o2 = new Order();
+		o2.setDateorder(LocalDate.now());
+		o2.setStatusorder(Status.INPROGRESS);
+		o2.setClient(c);
+		o2.setTotalorder(300.0);
+		o2 = orderRepository.save(o2);
+		Order o3 = new Order();
+		o3.setDateorder(LocalDate.now());
+		o3.setStatusorder(Status.INPROGRESS);
+		o3.setClient(c);
+		o3.setTotalorder(300.0);
+		o3 = orderRepository.save(o3);
 
+	}
+
+	@Test
+	@DisplayName("Test Delete Order")
+	void testdeleteOrder() {
+		Order o = orderRepository.findAll().stream().findFirst().orElse(null);
+		orderServiceImpl.deleteOrder(o.getIdorder());
+		Integer size = orderRepository.findAll().size();
+		assertEquals(2, size);
+	}
+
+	@Test
+	@DisplayName("Test Get Order")
+	void getOrder() {
+		Order firstOrder = orderRepository.findAll().stream().findFirst().orElse(null);
+		Order o = orderServiceImpl.getOrder(firstOrder.getIdorder());
+		assertNotNull(o);
+	}
+
+	@Test
+	@DisplayName("Test Get All Orders")
+	void getOrders() {
+		List<Order> orders = orderServiceImpl.getOrders();
+		assertEquals(3, orders.size());
+		assertNotNull(orders);
 	}
 
 	@Test
@@ -45,82 +95,58 @@ class OrderServiceImplTest {
 		o.setStatusorder(Status.INPROGRESS);
 		o.setClient(c);
 		o.setTotalorder(300.0);
-		assertNotNull(orderRepository.save(o));
+		orderServiceImpl.saveOrder(o);
+		assertEquals(4, orderRepository.findAll().size());
 
 	}
-	@Test
-	@DisplayName("test delete Order")
-	void testDeleteOrder() {
-		Client c = new Client();
-		c.setNameclient("yassine");
-		c.setIdclient(1);
-		c.setEmailclient("yassine@gmail.com");
 
-		Order o = new Order();
-		o.setDateorder(LocalDate.now());
-		o.setStatusorder(Status.INPROGRESS);
-		o.setClient(c);
-		o.setTotalorder(300.0);
-		o = orderRepository.save(o);
-		orderRepository.deleteById(o.getIdorder());
-		Order finalO = o;
-		assertThrows(NullPointerException.class,()->{orderServiceImpl.getOrder(finalO.getIdorder());});
-//		assertNull(orderServiceImpl.getOrder(2L));
+	@Nested
+	@ContextConfiguration(classes = { PersistenceJPAConfig.class })
+	class rapports {
+		@Test
+		@DisplayName("test get Total Orders Between other Dates")
+		void getorderdate2() {
+			Client c = new Client();
+			c.setIdclient(1);
+			Order o1 = new Order();
+			o1.setDateorder(LocalDate.of(2022, 12, 23));
+			o1.setStatusorder(Status.INPROGRESS);
+			o1.setClient(c);
+			o1.setTotalorder(300.0);
+			o1 = orderRepository.save(o1);
+			Order o2 = new Order();
+			o2.setDateorder(LocalDate.of(2022, 12, 23));
+			o2.setStatusorder(Status.INPROGRESS);
+			o2.setClient(c);
+			o2.setTotalorder(300.0);
+			o2 = orderRepository.save(o2);
+			Order o3 = new Order();
+			o3.setDateorder(LocalDate.of(2022, 12, 23));
+			o3.setStatusorder(Status.INPROGRESS);
+			o3.setClient(c);
+			o3.setTotalorder(300.0);
+			o3 = orderRepository.save(o3);
+			List<rapportData> ordeRapportDatas = orderServiceImpl.getTotalOrdersBetweenDates(LocalDate.of(2022, 12, 23),
+					LocalDate.now());
+			assertEquals(900, ordeRapportDatas.get(0).getTotal());
+			assertEquals(900, ordeRapportDatas.get(1).getTotal());
+			assertEquals(2, ordeRapportDatas.size());
+		}
+
+		@Test
+		@DisplayName("test get Total Orders Between Dates")
+		void getorderdate1() {
+			List<rapportData> ordeRapportDatas = orderServiceImpl.getTotalOrdersBetweenDates(LocalDate.now(),
+					LocalDate.now());
+			assertEquals(900, ordeRapportDatas.get(0).getTotal());
+			assertEquals(1, ordeRapportDatas.size());
+		}
 	}
-//	@Test
-//	@DisplayName("test get Order")
-//	void testGetOrder() {
-//		Client c = new Client();
-//		c.setIdclient(1);
-//
-//		Order o1 = new Order();
-//		o1.setDateorder(LocalDate.now());
-//		o1.setStatusorder(Status.INPROGRESS);
-//		o1.setClient(c);
-//		o1.setTotalorder(300.0);
-//		o1 = orderRepository.save(o1);
-//
-//		Order o2 = new Order();
-//		o2.setDateorder(LocalDate.now());
-//		o2.setStatusorder(Status.INPROGRESS);
-//		o2.setClient(c);
-//		o2.setTotalorder(300.0);
-//		o2 = orderRepository.save(o2);
-//		Order o = new Order();
-//		o.setDateorder(LocalDate.now());
-//		o.setStatusorder(Status.INPROGRESS);
-//		o.setClient(c);
-//		o.setTotalorder(300.0);
-//		o = orderRepository.save(o);
-//
-//
-//		assertEquals(3,orderServiceImpl.getOrders().size());
-//	}
 
-	@Test
-	@DisplayName("test get Order")
-
-	public void testGetOrder() {
-		Order order = new Order();
-		orderServiceImpl.save(order);
-		Order retrievedOrder = orderRepository.findById(order.getIdorder()).orElse(null);
-		assertEquals(order, retrievedOrder);}
-
-//	@Test
-//	@DisplayName("test get Orders")
-//
-//	public void testGetOrders() {
-//		Order order1 = new Order();
-//		// set properties of order1
-//		orderRepository.save(order1);
-//
-//		Order order2 = new Order();
-//		// set properties of order2
-//		orderRepository.save(order2);
-//
-//		List<Order> orders = orderRepository.findAll();
-//		assertTrue(orders.contains(order1));
-//		assertTrue(orders.contains(order2));
-//	}
+	@AfterEach
+	@DisplayName("Cleanup Test")
+	void cleanupTest() {
+		orderRepository.deleteAll();
+	}
 
 }
